7de2fe10c65fd48370755e93c722bdd158937779
marktipton <marktipton95@gmail.com>
2024-09-07 14:42:19 -0500

    added templates for components

---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoverArt = () => {
+  return (
+    <div>CoverArt</div>
+  )
+}
+
+export default CoverArt
---
+++ src/components/PlayControls.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayControls = () => {
+  return (
+    <div>PlayControls</div>
+  )
+}
+
+export default PlayControls
