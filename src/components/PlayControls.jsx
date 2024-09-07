4e0b8ee5cc54933dc55713b781a1f867aa7adc2d
marktipton <marktipton95@gmail.com>
2024-09-07 17:01:40 -0500

    added react icons. Play and replay are not exact

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { IoPlayBack } from "react-icons/io5";
+import { IoPlay } from "react-icons/io5";
+import { IoPlayForward } from "react-icons/io5";
+import { GrPowerCycle } from "react-icons/gr";
 
 const PlayControls = () => {
   return (
-    <div>
+    <div className='flex'>
       <p>1x</p>
+      <IoPlayBack/>
+      <IoPlay/>
+      <IoPlayForward/>
+      <GrPowerCycle/>
     </div>
   )
 }
