ae1f5ab7acf614864b49f5bef61555da48384c9e
marktipton <marktipton95@gmail.com>
2024-09-08 17:54:05 -0500

    correct play and next button

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -11,8 +11,32 @@ const PlayControls = () => {
       <button>
         <IoPlayBack className="text-gray-500"/>
       </button>
-      <button className='outline-1'><IoPlay/></button>
-      <button><IoPlayForward/></button>
+      <button className='outline-1'>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+          />
+        </svg>
+      </button>
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
+        </svg>
+      </button>
       <button><GrPowerCycle/></button>
     </div>
   )
