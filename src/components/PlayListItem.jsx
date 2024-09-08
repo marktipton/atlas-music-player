c245ab12b6765737bf0889e9e5a4a389216561a5
marktipton <marktipton95@gmail.com>
2024-09-08 15:48:03 -0500

    correct spacing and justification for playlist and playlist items

--- src/components/PlayListItem.jsx
+++ src/components/PlayListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PlayListItem = ({name, artist, time}) => {
   return (
-    <div className='flex space-x-10 items-center'>
+    <div className='flex w-full items-center justify-between'>
       <div>
         <p className='text-black font-medium'>{name}</p>
         <p className='text-gray-500 font-medium'>{artist}</p>
--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -3,7 +3,7 @@ import PlayListItem from './PlayListItem'
 
 const Playlist = () => {
   return (
-    <div className='p-6'>
+    <div className='p-6 md:w-1/2'>
       <h1 className='font-medium text-xl'>Playlist</h1>
       <PlayListItem name='Painted in Blue' artist='Soul Canvas' time='3:21'/>
       <PlayListItem name='Painted in Blue' artist='Soul Canvas' time='3:21'/>
