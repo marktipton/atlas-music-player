02d660e543386df46b9f3c09feafc435737e7427
marktipton <marktipton95@gmail.com>
2024-09-08 13:48:08 -0500

    added sound icon and slider

--- src/components/VolumeControl.jsx
+++ src/components/VolumeControl.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { HiMiniSpeakerWave } from "react-icons/hi2";
+// import { HiMiniSpeakerXMark } from "react-icons/hi2";
+import { Slider } from "@material-tailwind/react";
 
 const VolumeControl = () => {
   return (
-    <div>VolumeControl</div>
+    <div className='flex'>
+      <HiMiniSpeakerWave/>
+      <Slider color="blue-gray" />
+    </div>
   )
 }
 
